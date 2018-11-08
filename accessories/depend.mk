692727c294a7134009e001fa90fcd25aebaabd54
Adam Jirasek <Adam.Jirasek@gmail.com>
2018-11-08 16:31:19 -0700

    add python script

---
+++ accessories/depend.mk
@@ -0,0 +1,12 @@
+# This file is generated automatically by fort_depend.py. DO NOT EDIT!
+#
+#  Created by: jiraseka
+#  Date: 2018-11-08 06:11:04
+#
+
+fll_ugrid2fll.o :  \
+	../../data_util/fll_mods.o \
+	readugrid.o
+
+readugrid.o :  \
+	../../data_util/fll_mods.o
