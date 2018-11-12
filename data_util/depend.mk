5c0d25f13f08d1de37071fb3c9342dbce5ff6651
Adam Jirasek <Adam.Jirasek@gmail.com>
2018-11-12 09:35:07 -0700

    add fll_read_ugrid

--- data_util/depend.mk
+++ data_util/depend.mk
@@ -1,166 +1,172 @@
 # This file is generated automatically by fort_depend.py. DO NOT EDIT!
 #
-#  Created by: jiraseka
-#  Date: 2017-12-13 09:29:06
+#  Created by: jka
+#  Date: 2018-11-12 09:22:42
 #
-
-fll_deattach.o :  \
-	fll_stich.o \
-	fll_out.o \
-	fll_type.o
-
-fll_nnodes.o :  \
-	fll_funct_prt.o \
-	fll_out.o \
-	fll_type.o
-
-fll_getnbytes.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_mkdir.o :  \
-	fll_mk.o \
-	fll_type.o
-
-fll_cat.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_type.o : 
-
-fll_stich.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_funct_prt.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_sweep.o :  \
-	fll_locate.o \
-	fll_match_pattern.o \
-	fll_out.o \
-	fll_type.o
-
-fll_read_record.o :  \
-	fll_mv.o \
-	fll_cat.o \
-	fll_out.o \
-	fll_locate.o \
-	fll_rm.o \
-	fll_mk.o \
-	fll_type.o \
-	fll_read.o
-
-fll_read.o :  \
-	fll_mv.o \
-	fll_funct_prt.o \
-	fll_mk.o \
-	fll_out.o \
-	fll_type.o
-
-fll_cp.o :  \
-	fll_mv.o \
-	fll_cat.o \
-	fll_out.o \
-	fll_duplicate.o \
-	fll_rm.o \
-	fll_stich.o \
-	fll_type.o
 
 fll_scan_file.o :  \
 	fll_read.o \
-	fll_out.o \
-	fll_type.o
-
-fll_rename.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_mv.o :  \
-	fll_stich.o \
-	fll_out.o \
-	fll_rm.o \
-	fll_type.o
-
-fll_write.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_getndata.o :  \
-	fll_locate.o \
-	fll_out.o \
-	fll_type.o
-
-fll_match_pattern.o :  \
-	fll_out.o \
-	fll_type.o
-
-fll_rm.o :  \
-	fll_stich.o \
-	fll_out.o \
-	fll_type.o
-
-fll_read_ffa.o :  \
-	fll_mv.o \
-	fll_funct_prt.o \
-	fll_mk.o \
-	fll_out.o \
-	fll_type.o
-
-fll_locate.o :  \
-	fll_funct_prt.o \
-	fll_out.o \
-	fll_type.o
-
-fll_duplicate.o :  \
-	fll_mv.o \
-	fll_mk.o \
-	fll_out.o \
-	fll_type.o
-
-fll_mods.o :  \
-	fll_mv.o \
-	fll_out.o \
-	fll_funct_prt.o \
-	fll_stich.o \
-	fll_match_pattern.o \
 	fll_type.o \
-	fll_mkdir.o \
-	fll_cat.o \
-	fll_write.o \
-	fll_rename.o \
-	fll_read_record.o \
-	fll_write_ffa.o \
-	fll_getnbytes.o \
-	fll_mk.o \
-	fll_deattach.o \
-	fll_scan_file.o \
-	fll_duplicate.o \
-	fll_sweep.o \
-	fll_rm.o \
-	fll_read_ucd.o \
-	fll_nnodes.o \
-	fll_getndata.o \
-	fll_read_ffa.o \
-	fll_locate.o \
-	fll_cp.o \
-	fll_read.o
-
-fll_read_ucd.o :  \
-	fll_mkdir.o \
-	fll_mv.o \
-	fll_mk.o \
-	fll_out.o \
-	fll_type.o
+	fll_out.o
 
 fll_mk.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_read.o :  \
+	fll_type.o \
+	fll_funct_prt.o \
+	fll_mv.o \
 	fll_out.o \
-	fll_type.o
+	fll_mk.o
+
+fll_locate.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_funct_prt.o
 
 fll_out.o :  \
 	fll_type.o
 
-fll_write_ffa.o :  \
+fll_mods.o :  \
+	fll_read_ugrid.o \
+	fll_type.o \
+	fll_locate.o \
+	fll_duplicate.o \
+	fll_rename.o \
+	fll_cat.o \
+	fll_write.o \
+	fll_read_ucd.o \
+	fll_deattach.o \
+	fll_rm.o \
 	fll_out.o \
-	fll_type.o
+	fll_read_record.o \
+	fll_mkdir.o \
+	fll_getnbytes.o \
+	fll_match_pattern.o \
+	fll_cp.o \
+	fll_mk.o \
+	fll_funct_prt.o \
+	fll_read.o \
+	fll_getndata.o \
+	fll_write_ffa.o \
+	fll_read_ffa.o \
+	fll_stich.o \
+	fll_mv.o \
+	fll_nnodes.o \
+	fll_scan_file.o \
+	fll_sweep.o
+
+fll_read_ucd.o :  \
+	fll_type.o \
+	fll_mkdir.o \
+	fll_mv.o \
+	fll_out.o \
+	fll_mk.o
+
+fll_stich.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_duplicate.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_mv.o \
+	fll_mk.o
+
+fll_match_pattern.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_nnodes.o :  \
+	fll_out.o \
+	fll_type.o \
+	fll_funct_prt.o
+
+fll_cat.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_write.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_read_ffa.o :  \
+	fll_type.o \
+	fll_funct_prt.o \
+	fll_mv.o \
+	fll_out.o \
+	fll_mk.o
+
+fll_funct_prt.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_getnbytes.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_mkdir.o :  \
+	fll_type.o \
+	fll_mk.o
+
+fll_cp.o :  \
+	fll_type.o \
+	fll_duplicate.o \
+	fll_stich.o \
+	fll_cat.o \
+	fll_mv.o \
+	fll_rm.o \
+	fll_out.o
+
+fll_sweep.o :  \
+	fll_type.o \
+	fll_out.o \
+	fll_locate.o \
+	fll_match_pattern.o
+
+fll_write_ffa.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_mv.o :  \
+	fll_type.o \
+	fll_stich.o \
+	fll_out.o \
+	fll_rm.o
+
+fll_deattach.o :  \
+	fll_type.o \
+	fll_out.o \
+	fll_stich.o
+
+fll_rename.o :  \
+	fll_type.o \
+	fll_out.o
+
+fll_read_ugrid.o :  \
+	fll_type.o \
+	fll_out.o \
+	fll_mv.o
+
+fll_type.o : 
+
+fll_getndata.o :  \
+	fll_type.o \
+	fll_out.o \
+	fll_locate.o
+
+fll_read_record.o :  \
+	fll_type.o \
+	fll_locate.o \
+	fll_cat.o \
+	fll_mv.o \
+	fll_rm.o \
+	fll_read.o \
+	fll_out.o \
+	fll_mk.o
+
+fll_rm.o :  \
+	fll_type.o \
+	fll_out.o \
+	fll_stich.o
